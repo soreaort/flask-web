2c5cbd127fba99e393e6793b13222a5890f0d6b7
Saul Orea <soreaort@gmail.com>
2019-11-30 19:32:10 -0600

    Listen from VIP

--- web.py
+++ web.py
@@ -8,4 +8,4 @@ def home():
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
+    app.run(host="vmetcd-1.mgt.depa.mx", port=5000, threaded=True, debug=True)
